730fa9bd2767c3afef6401e4259a1bbd4e4be0a5
Syukansha <syukriishak622@gmail.com>
2022-06-15 00:11:28 +0800

    update

--- session.php
+++ session.php
@@ -1,10 +1,12 @@
 <?php
    include('connectDB.php');
    session_start();
+
+   $user_id = $_SESSION['userid'];
    
    $user_check = $_SESSION['login_user'];
    
-   $ses_sql = mysqli_query($conn,"select admin_name from admin where admin_name = '$user_check' ");
+   $ses_sql = mysqli_query($conn,"select admin_name from admin where admin_name = '$user_check' and admin_id = $user_id");
    
    $row = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
    
